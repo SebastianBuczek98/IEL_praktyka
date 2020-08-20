794be7f5f18830c41f1dd57e739c294e3ff02ee1
SebastianBuczek98 <sbuczek98@gmail.com>
2020-08-20 15:16:32 +0200

    wykresy:test

--- PostgreControl/main.py
+++ PostgreControl/main.py
@@ -1,4 +1,6 @@
 import psycopg2
+import matplotlib.pyplot as plt
+
 conn = psycopg2.connect(user="guest", password="guest", database="IEL", host="10.10.6.217", port="5432")
 print("Connected to: ", conn)
 
@@ -43,8 +45,25 @@ def display_all():
 
 
 def rysujWykres():
-    print("Wpisz okres: ")
-    
+    print("Okres czasu od (YYYY-MM-DD GG:MM:SS): ")
+    date_start = input()                #Pobranie daty początkowej
+    print("Do (YYYY-MM-DD GG:MM:SS): ")
+    date_end = input()                  #Pobranie daty końcowej
+    try:
+        cursor.execute("SELECT * FROM PUBLIC.\"TABLE1\" WHERE \"date\" between (%s) and (%s)", [date_start, date_end])  #backslashe są konieczne aby cudzysłowy nie były rozpoznawane jako znaki specjalne dla pythona, a dla postgre
+        date = cursor.fetchall()
+        for row in date:            #wyświetlenie rekordow (dla sprawdzenia poprawnosci dzialania)
+            print("ID: ", row[0])
+            print("Temp: ", row[1])
+            print("Date: ", row[2], "\n")
+    except (Exception, psycopg2.Error, TypeError) as error3:
+        print(error3)
+
+
+def wykres():
+    plt.plot([1, 2, 3, 4], [5, 6, 7, 7])
+    plt.show()
+
 
 menuloop = True
 while menuloop:
@@ -60,6 +79,8 @@ while menuloop:
         display_all()
     elif choice == '3':
         rysujWykres()
+    elif choice == '4':
+        wykres()
     elif choice == '0':
         menuloop = False
 
